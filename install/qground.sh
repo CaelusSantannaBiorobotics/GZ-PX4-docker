5888bea59b03a34c5c0efb22d752982d4d67e4ac
Sonic803 <sonic82003@gmail.com>
2024-03-26 18:39:31 +0100

    fix_qground.sh

--- install/qground.sh
+++ install/qground.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get remove modemmanager -y
 sudo DEBIAN_FRONTEND=noninteractive apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
 sudo DEBIAN_FRONTEND=noninteractive apt install libfuse2 -y
 sudo DEBIAN_FRONTEND=noninteractive apt install libxcb-xinerama0 libxkbcommon-x11-0 libxcb-cursor0 -y
